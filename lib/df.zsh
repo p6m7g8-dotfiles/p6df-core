5311491bf9e46632689f2935323f1af333e2b78e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-07-16 16:29:58 -0400

    fix: adds missing function and file load, post relocation

--- lib/df.zsh
+++ lib/df.zsh
@@ -10,7 +10,7 @@
 #  Environment:	 EOF
 #>
 #/ Synopsis
-#/    bin/p6ctl [-D|-d] [cmd]
+#/    bin/p6df [-D|-d] [cmd]
 #/
 ######################################################################
 p6df_usage() {
@@ -21,7 +21,7 @@ p6df_usage() {
     p6_msg "$msg"
   fi
   cat <<EOF
-Usage: bin/p6ctl [-D|-d] [cmd]
+Usage: bin/p6df [-D|-d] [cmd]
 
 Options:
   -D    debeug off
@@ -37,6 +37,71 @@ EOF
   exit "$rc"
 }
 
+######################################################################
+#<
+#
+# Function: p6_function_p6df(...)
+#
+#  Args:
+#	... -
+#
+#  Environment:	 LC_ALL OPTIND
+#>
+#/ Synopsis
+#/    bin/p6df [-D|-d] [cmd]
+#/
+#/ Synopsis
+#/    The entry point for bin/p6df
+#/
+######################################################################
+p6_function_p6df() {
+  shift 0
+
+  # sanitize env
+  LC_ALL=C
+
+  # default options
+  local flag_debug=0
+
+  # parse options
+  local flag
+  while getopts "dD" flag; do
+    case $flag in
+    D) flag_debug=0 ;;
+    d) flag_debug=1 ;;
+    *) p6df_usage 1 "invalid flag" ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  # grab command
+  local cmd="$1"
+  shift 1
+
+  # security 101: only allow valid comamnds
+  case $cmd in
+  help) p6df_usage ;;
+  doc) ;;
+  module) ;;
+  *) p6df_usage 1 "invalid cmd" ;;
+  esac
+
+  # setup -x based on flag_debug
+  [ ${flag_debug} = 1 ] && set -x
+
+  # exit if any cli errors w/ >0 return code
+  # the commands can still disable locally if needed
+  set -e
+  p6_msg "$cmd"
+  p6_cmd_df_"${cmd}" "$@"
+  p6_msg_success "$cmd"
+  set +e
+
+  # stop debugging if it was enabled
+  [ ${flag_debug} = 1 ] && set +x
+
+  return 0
+}
 ######################################################################
 #<
 #
@@ -153,4 +218,3 @@ p6_cmd_df_module_langs() {
   p6df::core::module::use "$module"
   p6df::core::module::langs "$module"
 }
-
