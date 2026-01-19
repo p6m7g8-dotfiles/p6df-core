c58f018651bed22694b4c8c092197009bcee71af
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 09:34:17 -0500

    chore: consolidate ::prompt::lang

--- lib/lang.zsh
+++ lib/lang.zsh
@@ -26,3 +26,45 @@ p6df::core::lang::mgr::init() {
 
     p6_return_void
 }
+
+######################################################################
+#<
+#
+# Function: str str = p6df::core::lang::prompt::lang(label, mgr_cmd, sys_cmd)
+#
+#  Args:
+#	label -
+#	mgr_cmd -
+#	sys_cmd -
+#
+#  Returns:
+#	str - str
+#
+#>
+######################################################################
+p6df::core::lang::prompt::lang() {
+    local label="$1"
+    local mgr_cmd="$2"
+    local sys_cmd="$3"
+
+    local ver_mgr
+    ver_mgr=$(eval "$mgr_cmd" 2>/dev/null)
+    if p6_string_blank "$ver_mgr"; then
+        ver_mgr="system"
+    fi
+
+    local ver
+    if p6_string_eq "$ver_mgr" "system"; then
+        local v
+        v=$(eval "$sys_cmd" 2>/dev/null)
+        if p6_string_blank "$v"; then
+            ver="sys:no"
+        else
+            ver="sys@$v"
+        fi
+    else
+        ver="$ver_mgr"
+    fi
+
+    p6_return_str "$label:$ver"
+}
